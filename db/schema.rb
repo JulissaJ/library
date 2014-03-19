17a1670425871c9874dcb1c27f5e486ed70e8314
JulissaJ <julissa.jansen@gmail.com>
2014-03-19 11:12:03 -0400

    added removed_favorites

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140319142110) do
+ActiveRecord::Schema.define(version: 20140319150057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "books", force: true do |t|
-    t.string   "title",                      null: false
+    t.string   "title",      null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "author",                     null: false
-    t.boolean  "favorite",   default: false, null: false
+    t.string   "author",     null: false
     t.integer  "rating"
   end
 
