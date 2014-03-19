9eae589b21e25cb017f2583a6d4d8ba4754ee3d4
JulissaJ <julissa.jansen@gmail.com>
2014-03-19 11:39:34 -0400

    added categories' '

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,6 +4,6 @@ class Book < ActiveRecord::Base
 	validates :author, presence: true
 	validates :rating, numericality: 
 		{greater_than_or_equal_to: 0,
-		 less_than_or_equal_to: 100
+		 less_than_or_equal_to: 100,
 		 allow_nil: true}
 end
